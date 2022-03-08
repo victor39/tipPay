60f146a575816990c9d5c5a26cdede431f6b949b
victor <valarcon@gmail.com>
2022-03-08 17:48:41 +0100

    dia-8.03.ULTIM

---
+++ app/src/main/java/com/example/tippay/registrarEmpresa.java
@@ -0,0 +1,14 @@
+package com.example.tippay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrarEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_empresa);
+    }
+}
---
+++ app/src/main/java/com/example/tippay/registrePersona.java
@@ -0,0 +1,14 @@
+package com.example.tippay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrePersona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registre_persona);
+    }
+}
