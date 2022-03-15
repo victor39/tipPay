f94f04db6b04ed30cf4f91b69da068e973b424c4
AuroraDiGiacomoCorrea <audico@inspalamos.cat>
2022-03-15 16:51:56 +0100

    aur

--- app/src/main/java/com/example/tippay/MainActivity.java
+++ app/src/main/java/com/example/tippay/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.tippay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    Connection connect;
+    String ConnnectionResult = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void intento(View view) {
+        try {
+            Connexio con = new Connexio();
+            connect = con.connexioClass();
+
+            if (connect != null) {
+                String query = "select * from persona";
+            }
+        }
+        catch(Exception ex){
+
+        }
     }
 }
