5208def03c558b03cf036794422e19a7af6d08c4
AuroraDiGiacomoCorrea <audico@inspalamos.cat>
2022-04-25 21:47:20 +0200

    aur

---
+++ app/src/main/java/clases/Treballador.java
@@ -0,0 +1,194 @@
+package clases;
+import android.app.Activity;
+import android.app.Application;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Treballador extends Persona{
+
+    public Treballador(String dni, String nom, String cognom1, String cognom2, LocalDate dataNaixement, String telf, String correu, String cp, String paypal, String compte_bancari) {
+        super(dni, nom, cognom1, cognom2, dataNaixement, telf, correu, cp, paypal, compte_bancari);
+    }
+
+    public static ArrayList<Treballador> tots(Activity act){
+        ArrayList<Treballador> treballadors = new ArrayList<Treballador>();
+
+        try {
+            String url = "https://ffames.cat/tippay/treballador-tots.php";
+            StringRequest postRequest = new
+                    //crear constructor
+                    StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    //devuelve el resultado de la consulta
+                                    //si hay un error de sintaxis en la consulta del php lo devolvera aqui
+                                    String resultado = response;
+                                    //while llenando
+
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    //si hay un error lo muestra
+                                    error.printStackTrace();
+
+                                }
+                            }
+                    ) {
+                        //generar clave-valor
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            // the POST parameters:
+                            return params;
+                        }
+                    };
+            //ejecutar y pasar parametros
+            Volley.newRequestQueue(act).add(postRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //una linea por trabajador
+        //while hasta que se acaben las
+        return treballadors;
+    }
+
+    public void insert(Activity act){
+
+        String dni = this.getDni();
+        String nom = this.getNom();
+        String cognom1 = this.getCognom1();
+        String cognom2 = this.getCognom2();
+        LocalDate datanaix = this.getDataNaixement();
+        String telefono = this.getTelf();
+        String correu = this.getCorreu();
+        String codipostal = this.getCp();
+        String paypal = this.getPaypal();
+        String comptebancari = this.getCompte_bancari();
+        try {
+            String url = "https://ffames.cat/tippay/Treballador-insert.php";
+            StringRequest postRequest = new
+                    //crear constructor
+                    StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    //devuelve el resultado de la consulta
+                                    //si hay un error de sintaxis en la consulta del php lo devolvera aqui
+
+                                    String resultado = response;
+
+
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    //si hay un error lo muestra
+                                    error.printStackTrace();
+                                    System.out.println("manel");
+                                }
+                            }
+                    ) {
+
+                        //generar clave-valor
+                        @Override
+                        protected Map<String, String> getParams() {
+
+                            Map<String, String> params = new HashMap<>();
+                            // the POST parameters:
+                            params.put("dni", dni);
+                            params.put("nom", nom);
+                            params.put("cognom1", cognom1);
+                            params.put("cognom2", cognom2);
+                            params.put("datanaix", datanaix.toString());
+                            params.put("telefono", telefono);
+                            params.put("correu", correu);
+                            params.put("codipostal", codipostal);
+                            params.put("paypal", paypal);
+                            params.put("comptebancari", comptebancari);
+                            return params;
+                        }
+                    };
+            //ejecutar y pasar parametros
+
+            Volley.newRequestQueue(act.getApplicationContext()).add(postRequest);
+            System.out.println("HOLA");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Activity act){
+        String dni = this.getDni();
+        String nom = this.getNom();
+        String cognom1 = this.getCognom1();
+        String cognom2 = this.getCognom2();
+        LocalDate datanaix = this.getDataNaixement();
+        String telefono = this.getTelf();
+        String correu = this.getCorreu();
+        String codipostal = this.getCp();
+        String paypal = this.getPaypal();
+        String comptebancari = this.getCompte_bancari();
+        try {
+            String url = "https://ffames.cat/tippay/treballador-insert.php";
+            StringRequest postRequest = new
+                    //crear constructor
+                    StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    //devuelve el resultado de la consulta
+                                    //si hay un error de sintaxis en la consulta del php lo devolvera aqui
+                                    String resultado = response;
+
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    //si hay un error lo muestra
+                                    error.printStackTrace();
+
+                                }
+                            }
+                    ) {
+                        //generar clave-valor
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            // the POST parameters:
+                            params.put("dni", dni);
+                            params.put("nom", nom);
+                            params.put("cognom1", cognom1);
+                            params.put("cognom2", cognom2);
+                            params.put("datanaix", datanaix.toString());
+                            params.put("telefono", telefono);
+                            params.put("correu", correu);
+                            params.put("codipostal", codipostal);
+                            params.put("paypal", paypal);
+                            params.put("comptebancari", comptebancari);
+                            return params;
+                        }
+                    };
+            //ejecutar y pasar parametros
+            Volley.newRequestQueue(act.getApplicationContext()).add(postRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/tippay/MainActivity.java
+++ app/src/main/java/com/example/tippay/MainActivity.java
@@ -1,25 +1,44 @@
 package com.example.tippay;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+
+import clases.Treballador;
 
 public class MainActivity extends AppCompatActivity {
 
     Connection connect;
     String ConnnectionResult = "";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String dni = "41533673C";
+        String nom = "Adrià";
+        String cognom1 = "Duran";
+        String cognom2 = "Portell";
+        LocalDate datanaix = LocalDate.of(1999,05,01);
+        String telefono = "722572442";
+        String correu = "aduran@inspalamos.cat";
+        String codipostal = "17250";
+        String paypal = "123345";
+        String comptebancari = "ES1233456789";
 
+        Treballador tre = new Treballador(dni, nom, cognom1, cognom2, datanaix, telefono, correu, codipostal, paypal, comptebancari);
+
+        tre.insert(MainActivity.this);
 
     }
 
