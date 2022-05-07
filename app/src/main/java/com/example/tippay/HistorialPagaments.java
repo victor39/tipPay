abe5c521fb0ec08a26f10ac5c46207ad7572ab0c
Duriman18 <aduran@inspalamos.cat>
2022-05-07 11:48:18 +0200

    Inici

--- app/src/main/java/com/example/tippay/HistorialPagaments.java
+++ app/src/main/java/com/example/tippay/HistorialPagaments.java
@@ -3,6 +3,7 @@ package com.example.tippay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class HistorialPagaments extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class HistorialPagaments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_pagaments);
+
+        emplenarTaula();
+
+    }
+
+    protected void emplenarTaula(){
+
+        TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+
+
     }
 }
