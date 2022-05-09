93458fa65f6bcbe93285c127dc535c8de4a00c91
AuroraDiGiacomoCorrea <audico@inspalamos.cat>
2022-05-09 22:43:18 +0200

    clases

--- app/src/main/java/com/example/tippay/registrePersona.java
+++ app/src/main/java/com/example/tippay/registrePersona.java
@@ -1,8 +1,10 @@
 package com.example.tippay;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +49,7 @@ public class registrePersona extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void returnPersona(View view) {
         Intent main = new Intent(this, MainActivity.class);
         startActivity(main);
